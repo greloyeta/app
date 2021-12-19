02f2dbacf5daab07cb5b1c402e900cf5ae87a081
greloyeta <96381686+greloyeta@users.noreply.github.com>
2021-12-19 21:44:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,22 @@
-
 import streamlit as st
-import pandas as pd
-import math
-import numpy as np
-import pandas_datareader as web
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
-import matplotlib.pyplot as plt
 
-st.markdown("""
-#Stock Predictor
-This website can predict the value of a stock price in the in the future
-""")
+from src.pages.page_main import run_page_main
+from src.pages.page_howto import run_page_howto
+from src.setup.layout import setup_page
 
-st.write("""Disclaimer: Any investing is done at the investors expense, we are not responsible for any actions taken.
-""")
-
-try:
-    ticker = st.text_input("Enter Ticker symbol below", "GOOGL")
-    df = web.DataReader(ticker, data_source='yahoo', start='2012-01-01', end='2019-12-17')
-
-except:
-    ticker = 'GOOGL'
-    df = web.DataReader(ticker, data_source='yahoo', start='2012-01-01', end='2019-12-17')
-
-#visualization
-st.line_chart(df['Close'])
-st.caption(ticker+' stock price.')
+title = "StravAnalyse"
+setup_page(title)
 
 
-valid, train, predictions = sp.make_model(df)
+with st.sidebar:
+    st.title(title)
+    st.header("")
+    # page = st.selectbox("Select page", ["Main", "How-to"])
+    # st.header("")
+    page = "Main"
 
-valid['Predictions'] = predictions
+if page == "Main":
+    run_page_main()
 
-"""
-Actual Price vs Predicted
-"""
-st.line_chart(valid)
+elif page == "How-to":
+    run_page_howto()
