0a480ab3c7eeeefd1a423a9f43094aeca21a5fc5
greloyeta <96381686+greloyeta@users.noreply.github.com>
2021-12-19 21:50:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,141 @@
-import pandas as pd
-import numpy as np
-import pickle
+from time import sleep
+from typing import List
+
 import streamlit
 
-model=pickle.load(open('model.pkl', 'rb'))
-def bc_prediction(var_1,var_2,var_3,var_4,var_5,var_6,var_7,var_8,var_9,var_10):
-    pred_arr=np.array([var_1,var_2,var_3,var_4,var_5,var_6,var_7,var_8,var_9,var_10]) 
-    preds=pred_arr.reshape(1,-1)
-    preds=preds.astype(int)
-    model_prediction=model.predict(preds)
-    return model_prediction
+from pizza_lib import (
+    PizzaCrust, PizzaSauce, PizzaTopping, Pizza, PizzaOrder,
+    PIZZA_SIZES,
+    PIZZA_CRUSTS, PIZZA_SAUCES, PIZZA_TOPPINGS
+)
 
-def run():
+streamlit.set_page_config(
+    page_title="MegaPizza",
+    page_icon="pizza",
+    layout="centered"
+)
 
+streamlit.title("MegaPizza")
 
-    streamlit.title("")
-    html_temp=""" 
-    """
-    streamlit.markdown(html_temp)
-var_1=streamlit.text_input("Variable 1")
-var_2=streamlit.text_input("Variable 2")
-var_3=streamlit.text_input("Variable 3")
-var_4=streamlit.text_input("Variable 4")
-var_5=streamlit.text_input("Variable 5")
-var_6=streamlit.text_input("Variable 6")
-var_7=streamlit.text_input("Variable 7")
-var_8=streamlit.text_input("Variable 8")
-var_9=streamlit.text_input("Variable 9")
-var_10=streamlit.text_input("Variable 10")
+streamlit.header("Number of Pizza Configurations")
 
-prediction=""
-if streamlit.button('Predict'):
-        prediction=bc_prediction(var_1,var_2,var_3,var_4,var_5,var_6,var_7,var_8,var_9,var_10)
+NUMBER_PIZZA_CONFIGURATIONS = streamlit.number_input(
+    "Select Number of Pizza Configurations",
+    min_value=0,
+    max_value=10,
+    step=1,
+    value=1
+)
 
-streamlit.success(f'The prediction by Model: {prediction}')
+PIZZAS: List[Pizza] = []
 
-if __name__=='__main__':
-    run()
+if NUMBER_PIZZA_CONFIGURATIONS == 0:
+    streamlit.info("We can't make zero Pizzas!")
+else:
+    streamlit.header("Configure your MegaPizza")
+
+for pizza_id in range(
+        1,
+        NUMBER_PIZZA_CONFIGURATIONS + 1
+):
+    streamlit.subheader(f"Pizza #{pizza_id}")
+
+    pizza_size = streamlit.select_slider(
+        "Select a Pizza Size",
+        PIZZA_SIZES,
+        format_func=lambda size: f"⌀{size}cm",
+        key=f"pizza_size#{pizza_id}"
+    )
+
+    pizza_crust = streamlit.radio(
+        "Select a Crust Type",
+        [
+            crust
+            for crust in PIZZA_CRUSTS
+            if crust.diameter == pizza_size
+        ],
+        key=f"pizza_crust#{pizza_id}"
+    )
+
+    streamlit.image(pizza_crust.image)
+
+    pizza_sauce = streamlit.radio(
+        "Select a Sauce",
+        [None] + [
+            sauce
+            for sauce in PIZZA_SAUCES
+            if sauce.diameter == pizza_size
+        ],
+        key=f"pizza_sauce#{pizza_id}"
+    )
+
+    if pizza_sauce:
+        streamlit.image(pizza_sauce.image)
+
+    pizza_toppings = streamlit.multiselect(
+        "Select one or multiple Toppings",
+        [
+            topping
+            for topping in PIZZA_TOPPINGS
+            if topping.diameter == pizza_size
+        ],
+        key=f"pizza_toppings#{pizza_id}"
+    )
+
+    for pizza_topping in pizza_toppings:
+        streamlit.image(pizza_topping.image)
+
+    pizza_name = streamlit.text_input(
+        "Pizza Name",
+        key=f"pizza_name#{pizza_id}"
+    )
+
+    pizza_number = streamlit.number_input(
+        "How many?",
+        min_value=1,
+        max_value=12,
+        value=1,
+        step=1,
+        key=f"pizza_number#{pizza_id}"
+    )
+
+    for index in range(pizza_number):
+        pizza = Pizza(
+            pizza_name,
+            pizza_crust,
+            pizza_sauce,
+            *pizza_toppings
+        )
+
+        streamlit.write(pizza)
+
+        PIZZAS.append(
+            pizza
+        )
+
+streamlit.header("Make MegaPizza Order")
+
+customer_name = streamlit.text_input("Customer Name")
+
+customer_phone_number = streamlit.text_input("Customer Phone Number")
+
+PIZZA_ORDER = PizzaOrder(
+    customer_name=customer_name,
+    customer_phone_number=customer_phone_number,
+    pizzas=PIZZAS
+)
+
+if streamlit.button(f"Authorize Payment of {PIZZA_ORDER.price:.2f}€"):
+
+    for pizza in PIZZA_ORDER:
+        streamlit.subheader(pizza.name)
+        pizza_progress = streamlit.progress(0)
+
+        with streamlit.spinner("Baking Pizza"):
+            for progress, waiting_time in enumerate(pizza.bake()):
+                sleep(waiting_time)
+                pizza_progress.progress(progress)
+
+        streamlit.image(pizza.image, caption=pizza.name)
+
+        streamlit.balloons()
